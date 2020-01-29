f29c29b3193b72b1cb7c91a3251e184e551b8783
imorti <imorti@gmail.com>
2020-01-29 11:57:53 -0800

    dealing with exceptions

---
+++ functions/exceptions.py
@@ -0,0 +1,11 @@
+try:
+    age = int(input("Age:"))
+    income = 20000
+    risk = income /age
+    print(age)
+
+except ValueError:
+    print("Invalid value")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
