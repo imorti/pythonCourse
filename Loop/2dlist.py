931dfc7c604a2164f695891ff425026092ea653d
imorti <imorti@gmail.com>
2020-01-29 09:36:00 -0800

    fun with 2d lists

---
+++ Loop/2dlist.py
@@ -0,0 +1,18 @@
+# [
+#     1 2 3
+#     4 5 6
+#     7 8 9
+# ]
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+#accessing a certain item in a matrix
+# matrix[0][1] = 20
+# print(matrix[0][1])
+
+for row in matrix:
+    for item in row:
+        print(item)
