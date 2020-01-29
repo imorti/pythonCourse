deee5aa0a03fb5a2a2c715339bf66620be142ccb
imorti <imorti@gmail.com>
2020-01-29 12:21:58 -0800

    fun with inheritance

---
+++ Classes/inheritence.py
@@ -0,0 +1,21 @@
+class Mammal:
+    def walk(self):
+        print("walking")
+
+class Dog(Mammal):
+    def bark(self):
+        print("bark")
+
+
+class Cat(Mammal):
+    def be_annoying(self):
+        print("annoying")
+
+
+
+dog1 = Dog()
+dog1.walk()
+dog1.bark()
+
+cat1 = Cat()
+cat1.be_annoying()
