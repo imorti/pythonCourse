19ce1e1940104c2880207fc55dcd9d38d3a6a158
imorti <imorti@gmail.com>
2020-01-29 11:09:38 -0800

    more fun with functions

--- functions/functions3.py
+++ functions/functions3.py
@@ -5,5 +5,6 @@ def greet_user(first_name, last_name):
 
 print("Start")
 greet_user(last_name="Smith", first_name="John")
+calc_cost(total=50, shipping=5, discount=0.1)
 greet_user("imorti", "Danny")
 print("Finish")
